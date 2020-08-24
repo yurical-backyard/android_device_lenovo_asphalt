55ebdd496d6b1fb60c83ae21512d282d285cf63d
Weilun Du <wdu@google.com>
2020-08-24 10:50:06 -0700

    asphalt: Declare Wi-Fi Passpoint Capability
    BUG: 165290461
    Test: run gts -m GtsGmscoreHostTestCases -t
    com.google.android.gts.wifi.WifiHostTest#testPasspointCapability
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia6e72b875956bf546de76ddd8ff9032effb54338

--- device.mk
+++ device.mk
@@ -125,3 +125,6 @@ PRODUCT_PACKAGES += \
 # Wi-Fi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/qca6490/WCNSS_qcom_cfg.ini
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml
