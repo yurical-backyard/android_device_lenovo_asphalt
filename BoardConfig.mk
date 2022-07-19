491fe0158a051923f62cb3a2456ca161425ae7ef
Jake Weinstein <jake@aospa.co>
2022-07-19 14:52:50 -0500

    asphalt: Remove console and video from cmdline
    These are unnecessary for production builds and are a waste
    of memory for our purposes.
    
    Change-Id: I9a52f989804ed9fd2d2e4ed53061281a408bd2fe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,7 @@ BOARD_BOOTCONFIG := \
 
 BOARD_KERNEL_CMDLINE := \
     disable_dma32=on \
-    video=vfb:640x400,bpp=32,memsize=3072000
+    msm_geni_serial.con_enabled=0
 
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
