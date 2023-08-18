a38d1994322faefadc1e55af332dae35ee59e981
Tim Zimmermann <tim@linux4.de>
2023-08-18 15:30:25 +0200

    asphalt: Include window_extensions.mk
    * For two panel settings
    
    Change-Id: Ia17ff75c82f47041f15ffc873dd78a5414b24aba

--- device.mk
+++ device.mk
@@ -20,6 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Window Extensions
+$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
