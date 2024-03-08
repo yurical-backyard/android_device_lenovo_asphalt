7275978bb2524540fca87198fab6c699c3b1fb94
Yurical <yurical1g@gmail.com>
2024-03-08 12:15:21 +0900

    asphalt: Import audio configs from LA.VENDOR.1.0.r2-09400-WAIPIO.QSSI14.0
    https://git.codelinaro.org/clo/la/platform/hardware/qcom/audio-ar/-/tree/LA.VENDOR.1.0.r2-09400-WAIPIO.QSSI14.0

--- device.mk
+++ device.mk
@@ -10,3 +10,11 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/lenovo/asphalt/asphalt-vendor.mk)
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/audio_effects.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_waipio_cdp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/mixer_paths_waipio_cdp.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_waipio_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/mixer_paths_waipio_mtp.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_waipio_qrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/mixer_paths_waipio_qrd.xml
