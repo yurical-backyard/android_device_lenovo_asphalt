12ab6b4ae744172c926f559097ab8e94fceb12c3
Yurical <yurical1g@gmail.com>
2024-03-08 21:16:08 +0900

    asphalt: Import wifi config from LA.VENDOR.1.0.r1-24600-WAIPIO.QSSI14.0
    https://git.codelinaro.org/clo/le/platform/vendor/qcom/wlan/-/tree/LA.VENDOR.1.0.r1-24600-WAIPIO.QSSI14.0

--- device.mk
+++ device.mk
@@ -49,3 +49,7 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.odm.rc \
     ueventd.qcom.rc
+
+# Wi-Fi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/qca6490/WCNSS_qcom_cfg.ini
