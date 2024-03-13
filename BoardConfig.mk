2931f01e27b9d45376bb5ae67c8eecc67a1e5162
Yurical <yurical1g@gmail.com>
2024-03-13 21:44:14 +0900

    asphalt: Define device-specific SEPolicy directory

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,3 +8,7 @@ DEVICE_PATH := device/lenovo/asphalt
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
+# SEPolicy
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
